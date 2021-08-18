0741d490cbc5f2573b1989c542f65abf61cce82d
Frederic Theriault <fred.theriault@gmail.com>
2021-08-18 09:34:40 -0400

    Refined removing wall logic

--- remove-banner.js
+++ remove-banner.js
@@ -1,2 +1,13 @@
-document.body.style.overflow = "auto";
-document.querySelector(".brz_msg_wall_body").remove();
+localStorage["lp.message.wall.active"] = "0";
+localStorage["lp.message.wall.countdown"] = "2";
+
+setTimeout(() => {
+	let wall = document.querySelector(".brz_msg_wall_body");
+	
+	if (wall != null) {
+		wall.parentNode.style.display = "none";
+		console.log("Banner removed");
+	}
+}, 500);
+
+
