9cd20e8909ad07eef538a4111dc1e5681b291fc5
Frederic Theriault <fred.theriault@gmail.com>
2021-08-25 10:35:11 -0400

    Added support on new website

--- remove-banner.js
+++ remove-banner.js
@@ -1,6 +1,18 @@
 localStorage["lp.message.wall.active"] = "0";
 localStorage["lp.message.wall.countdown"] = "2";
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+setCookie("pw6", "==", "1");
+
 setTimeout(() => {
 	let wall = document.querySelector(".brz_msg_wall_body");
 	
